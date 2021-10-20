71a40f092366ac9fc1aa81ebaf2dbcaab5bcd3fd
Peter Rubarth <p.rubarth+github@gmail.com>
2021-10-20 22:12:00 +0200

    Logik für Risiko Wüfeln teilweise umgesetzt. Formular umgesetzt

--- DiceThrow/public_html/js/main.js
+++ DiceThrow/public_html/js/main.js
@@ -11,3 +11,37 @@ function writeResult (max){
   var element = document.getElementById("result");
   element.innerHTML = "Zahl: "+result;
 }
+
+/*
+ * Formular Einheiten auswerten und verarbeiten
+ */
+function processUnitsInput (form){
+    let output = document.getElementById('output');
+    let attackerUnits = null;
+    attackerUnits = document.getElementById("attackerUnits");
+    
+    let defenderUnits = null;
+    defenderUnits = document.getElementById("defenderUnits");
+    
+
+    //null check on form
+    if (!form || !attackerUnits || !defenderUnits) {
+        output.innerHTML = "form is not vailid";
+        return;
+    }
+    
+    //roll dice for attacker
+    let diceAttacker = attackerUnits.value;
+    const attackerResults = [diceAttacker];
+ 
+    for (let i = 0; i < attackerResults.length; i++ ){
+        attackerResults [i] = rollDice (6);
+    }
+    //todo sortieren
+   
+   //todo defender
+    let diceDefender = defenderUnits.value;    
+    output.innerHTML = "Angreifer: "+attackerUnits.value+"</br>"+"Verteidiger: "+defenderUnits.value;
+    
+    //todo würfe vergleichen und ergebnis bestimmen
+}
