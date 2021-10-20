8d06cf2ce6ad1c1966aac5634d0a96534326173e
Peter Rubarth <p.rubarth+github@gmail.com>
2021-10-20 19:36:29 +0200

    Bugfix WÜrfel Risiko Würfler angefangen

--- DiceThrow/public_html/js/main.js
+++ DiceThrow/public_html/js/main.js
@@ -2,8 +2,12 @@
 function rollDice(max) {
   var dice = 0;
   var min = 1;
-
+  max = max+1;
   dice = Math.floor(Math.random() * (max - min)) + min;
+  return dice;
+}
+function writeResult (max){
+  var result = rollDice (max);
   var element = document.getElementById("result");
-  element.innerHTML = "Zahl: "+dice;
+  element.innerHTML = "Zahl: "+result;
 }
